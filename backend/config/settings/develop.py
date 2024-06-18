0c65d463d9eff063a750c301e8279d6d7b435895
Sergey <sergeylive87@yandex.ru>
2024-06-19 01:02:21 +0300

    Fix

--- backend/config/settings/develop.py
+++ backend/config/settings/develop.py
@@ -1,7 +1,7 @@
 from django.core.management.utils import get_random_secret_key
 
 from config.settings.base_settings import BASE_DIR
-from config.settings.base_settings import *
+# from config.settings.base_settings import *
 
 
 SECRET_KEY = get_random_secret_key()
