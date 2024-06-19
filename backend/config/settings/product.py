525a21f33baa5ae74cae2be7ab194866670c53b9
Sergey <sergeylive87@yandex.ru>
2024-06-20 01:27:03 +0300

    Fix

--- backend/config/settings/product.py
+++ backend/config/settings/product.py
@@ -2,7 +2,7 @@ from django.core.management.utils import get_random_secret_key
 
 import os
 
-from config.settings.base_settings import *
+# from config.settings.base_settings import *
 
 
 SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
