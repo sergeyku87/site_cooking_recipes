d1d5766190a0edaaa21808019cf2e5a0acb1d9b8
Sergey <sergeylive87@yandex.ru>
2024-06-20 08:50:20 +0300

    Fix

--- backend/tests/test_routes.py
+++ backend/tests/test_routes.py
@@ -4,6 +4,15 @@ from tests.fixtures import FixtureMixin
 
 
 class TestBegin(FixtureMixin):
+    def test_available_for_anonymous_client(self):
+        """
+        Check available user pages for not authenticated client.
+        """
+        for url in [self.users_url, self.users_id_url]:
+            with self.subTest(url=url):
+                response = self.client_without_token.get(self.users_url)
+                self.assertEqual(response.status_code, status.HTTP_200_OK)
+
     def test_available_for_client_with_token(self):
         """
         Check available user pages for authenticated client.
